047d589091f6807e228b4e217547ade93b553206
Attila Zsiros <attila.zsiros@outlook.sk>
2020-12-02 20:09:23 +0100

    Coin recognition for euro coins

--- main.py
+++ main.py
@@ -7,10 +7,26 @@ from matplotlib.widgets import Button
 import math
 
 BASE_DIR_PATH = "../IMAGES/Coins/CZK"
-CLASS = "10"
+CLASS = "2"
 LONGER_EDGE_SIZE = 1024
 WINDOW_NAME = 'FindCircles'
 COIN_SIZE = 180
+
+
+class Image:
+    def __init__(self, image_name, dir):
+        self.name = image_name
+        self.dir = dir
+        self.img_color = cv2.imread(dir + "/" + image_name)
+        self.img_gray = cv2.cvtColor(self.img_color, cv2.COLOR_BGR2GRAY)
+        self.rois = []
+
+    def resize(self):
+        w, h = self.img_color.shape[:2]
+        longer_edge = max(w, h)
+        scale_factor = LONGER_EDGE_SIZE / longer_edge
+        new_size = (int(h * scale_factor), int(w * scale_factor))
+        self.img_color = cv2.resize(self.img_color, new_size)
 
 
 def nothing(_):
@@ -30,17 +46,9 @@ def init_mpl():
         mpl.rcParams['keymap.save'].remove('s')
 
 
-def resize_image(img_color):
-    w, h = img_color.shape[:2]
-    longer_edge = max(w, h)
-    scale_factor = LONGER_EDGE_SIZE / longer_edge
-    new_size = (int(h * scale_factor), int(w * scale_factor))
-    return cv2.resize(img_color, new_size)
-
-
-def detect_circles(img_color, show_edges=False):
+def detect_circles(img, show_edges=False):
     # check dimensions
-    if max(img_color.shape[:2]) > LONGER_EDGE_SIZE:
+    if max(img.img_color.shape[:2]) > LONGER_EDGE_SIZE:
         raise Exception('image too large')
 
     while True:
@@ -50,21 +58,20 @@ def detect_circles(img_color, show_edges=False):
         acc_threshold = cv2.getTrackbarPos('AccTh', WINDOW_NAME)
 
         if show_edges:
-            img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
-            edges = cv2.Canny(img_gray, canny_high/2, canny_high)
+            edges = cv2.Canny(img.img_gray, canny_high / 2, canny_high)
             cv2.namedWindow("CannyWindow", cv2.WINDOW_NORMAL)
             cv2.imshow("CannyWindow", edges)
 
         # find circles
-        circles = find_circles_in_image(img_color, min_dist, canny_high, acc_threshold)
+        circles = find_circles_in_image(img, min_dist, canny_high, acc_threshold)
 
         # visualize
-        img_color_circles = draw_circles_into_image(img_color, circles)
+        img_color_circles = draw_circles_into_image(img, circles)
         cv2.imshow(WINDOW_NAME, img_color_circles)
 
         key = cv2.waitKey(1) % 256
         if key == ord('s'):
-            extract_circles(img_color, circles)
+            extract_circles(img, circles)
 
         if (key == ord('q')) or (key == ord('n')) or (key == ord('p')):
             break
@@ -72,9 +79,9 @@ def detect_circles(img_color, show_edges=False):
     return key
 
 
-def find_circles_in_image(img_color, min_dist, canny_high, acc_threshold, show_edges=True):
+def find_circles_in_image(img, min_dist, canny_high, acc_threshold, show_edges=True):
     # to grayscale
-    img_gray = cv2.cvtColor(img_color, cv2.COLOR_BGR2GRAY)
+    img_gray = cv2.cvtColor(img.img_color, cv2.COLOR_BGR2GRAY)
 
     # blur
     img_gray = cv2.GaussianBlur(img_gray, (7, 7), 1.5)
@@ -86,7 +93,7 @@ def draw_circles_into_image(img_color, circles):
     if circles is None:
         return img_color
 
-    img_with_circles = img_color.copy()
+    img_with_circles = img_color.img_color.copy()
 
     circles = np.uint16(np.round(circles))
     for (x, y, r) in circles[0, :]:
@@ -98,13 +105,18 @@ def draw_circles_into_image(img_color, circles):
     return img_with_circles
 
 
-def save_circles(rois):
-    print(len(rois))
+def save_circles(img):
+    print(len(img.rois))
+    for i in range(len(img.rois)):
+        path = img.dir + "/../" + str(i) + "_" + img.name
+        print("Save " + path)
+        # TODO save
+
+    print()
     plt.close('all')
 
 
 def extract_circles(img_color, circles):
-    rois = []
     circles_count = len(circles[0])
 
     # init figure
@@ -114,8 +126,8 @@ def extract_circles(img_color, circles):
     # create button
     axsave = plt.axes([0.89, 0.01, 0.1, 0.075])
     bsave = Button(axsave, 'Save')
-    bsave.on_clicked(lambda _: save_circles(rois))
-    fig.canvas.mpl_connect('key_release_event', lambda event: save_circles(rois) if event.key == 's' else nothing(0))
+    bsave.on_clicked(lambda _: save_circles(img_color))
+    fig.canvas.mpl_connect('key_release_event', lambda event: save_circles(img_color) if event.key == 's' else nothing(0))
 
     for i in range(fig_size * fig_size):
         if i >= circles_count:
@@ -123,8 +135,8 @@ def extract_circles(img_color, circles):
 
         x, y, r = circles[0][i]
         fig.add_subplot(fig_size, fig_size, i + 1)
-        roi = img_color[int(y-r):int(y+r), int(x-r):int(x+r)]
-        rois.append(roi)
+        roi = img_color.img_color[int(y - r):int(y + r), int(x - r):int(x + r)]
+        img_color.rois.append(roi)
         plt.imshow(cv2.cvtColor(roi, cv2.COLOR_BGR2RGB))
 
     plt.show()
@@ -138,13 +150,10 @@ def process_class(class_name):
     i = 0
     while i < len(class_image_names):
         image_name = class_image_names[i]
-        image_path = class_orig_dir_path + "/" + image_name
-
-        # open image
-        img = cv2.imread(image_path)
+        img = Image(image_name, class_orig_dir_path)
 
         # resize image
-        img = resize_image(img)
+        img.resize()
 
         # detect circles
         key = detect_circles(img, show_edges=True)
